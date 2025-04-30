62b188220ebe9f6422debd250c68d45584afaa52
Eli Davis <eli.davis1995@gmail.com>
2025-04-30 06:33:23 -0500

    fix wasm zip

--- generator/app_wasm.go
+++ generator/app_wasm.go
@@ -3,39 +3,37 @@
 package generator
 
 import (
-	"bytes"
 	"log"
-	"syscall/js"
 
 	wasmhttp "github.com/nlepage/go-wasm-http-server/v2"
 )
 
 var globalApp *App
 
-func wasmZip(this js.Value, cb []js.Value) interface{} { //
+// func wasmZip(this js.Value, cb []js.Value) interface{} { //
 
-	if globalApp == nil {
-		panic("global app not configured. Run app.Run(os.Args)")
-	}
+// 	if globalApp == nil {
+// 		panic("global app not configured. Run app.Run(os.Args)")
+// 	}
 
-	b := bytes.Buffer{}
-	err := globalApp.WriteZip(&b)
-	if err != nil {
-		log.Printf("error zipping: %s", err.Error())
-	}
+// 	b := bytes.Buffer{}
+// 	err := globalApp.WriteZip(&b)
+// 	if err != nil {
+// 		log.Printf("error zipping: %s", err.Error())
+// 	}
 
-	log.Printf("completed")
+// 	log.Printf("completed")
 
-	data := b.Bytes()
+// 	data := b.Bytes()
 
-	dst := js.Global().Get("Uint8Array").New(len(data))
-	js.CopyBytesToJS(dst, data)
-	cb[0].Invoke(dst)
-	return dst
-}
+// 	dst := js.Global().Get("Uint8Array").New(len(data))
+// 	js.CopyBytesToJS(dst, data)
+// 	cb[0].Invoke(dst)
+// 	return dst
+// }
 
 func os_setup(a *App) {
-	js.Global().Set("zipGeometry", js.FuncOf(wasmZip))
+	// js.Global().Set("zipGeometry", js.FuncOf(wasmZip))
 	globalApp = a
 }
 
